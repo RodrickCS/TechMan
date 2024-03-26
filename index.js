8fddcc5de2993e7d296346f40b93a17e08abf00e
Rodrigo Caetano Silva <98469493+RodrickCS@users.noreply.github.com>
2024-03-26 09:17:37 -0300

    Teste vercel

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-require("dotenv").config()
-const PORT = process.env.PORT || 3000
+ //require("dotenv").config()
+//const PORT = process.env.PORT || 3000
 const express = require("express")
-const methodOverride = require("method-override")
+//const methodOverride = require("method-override")
 
-const usuarioRoutes = require("./src/routes/usuarios_routes")
-const equipamentosRoutes = require("./src/routes/equipamentos_routes")
+// const usuarioRoutes = require("./src/routes/usuarios_routes")
+// const equipamentosRoutes = require("./src/routes/equipamentos_routes")
 
 const app = express()
-app
-  .set("view engine", "pug")
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
-  .use(express.static("public"))
-  .use(methodOverride("_method"))
-  .use(usuarioRoutes)
-  .use(equipamentosRoutes)
-app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+app.listen(3000, () => console.log("Server ready on port 3000."));
+// app
+ // .set("view engine", "pug")
+  //.use(express.json())
+  //.use(express.urlencoded({ extended: true }))
+ // .use(express.static("public"))
+ // .use(methodOverride("_method"))
+ // .use(usuarioRoutes)
+  //.use(equipamentosRoutes)
+// app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
 
 module.exports = app
